9efc35911b8d4ee8f771f78991ec5e7f1c858a17
Faheem <faheem@larasoft.io>
2023-04-10 14:59:47 +0500

    minor

--- Http/Controllers/Admin/IndexController.php
+++ Http/Controllers/Admin/IndexController.php
@@ -4,6 +4,7 @@ namespace Modules\Bing\Http\Controllers\Admin;
 
 use App\Http\Controllers\Admin\BaseAdminController;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Modules\Bing\Services\DataService;
 
@@ -34,12 +35,13 @@ class IndexController extends BaseAdminController
         $client = new DataService();
 
         $index_data = $client->post('index-data', [
+            'timezone' => Auth::user()->timezone,
             'start' => $this->dates['start']->format('Y-m-d H:i:s'),
             'end'   => $this->dates['end']->format('Y-m-d H:i:s')
         ]);
-        $campaigns = $index_data['campaigns'];
-        $spend = $index_data['spend'];
-        $statistics = $index_data['statistics'];
+        $campaigns = collect($index_data['campaigns']);
+        $spend = collect($index_data['spend']);
+        $statistics = collect($index_data['statistics']);
 
         return view('bing::admin.index')->with(compact('campaigns', 'spend', 'statistics'));
     }
@@ -49,14 +51,16 @@ class IndexController extends BaseAdminController
         $client = new DataService();
 
         $index_data = $client->post('campaign-data', [
+            'id'    => $id,
+            'timezone' => Auth::user()->timezone,
             'start' => $this->dates['start']->format('Y-m-d H:i:s'),
             'end'   => $this->dates['end']->format('Y-m-d H:i:s')
         ]);
 
         $campaign = $index_data['campaign'];
-        $adgroups = $index_data['adgroups'];
-        $spend = $index_data['spend'];
-        $statistics = $index_data['statistics'];
+        $adgroups = collect($index_data['adgroups']);
+        $spend = collect($index_data['spend']);
+        $statistics = collect($index_data['statistics']);
 
         return view('bing::admin.campaign')->with(compact('campaign', 'adgroups', 'spend', 'statistics'));
     }
@@ -66,14 +70,16 @@ class IndexController extends BaseAdminController
         $client = new DataService();
 
         $index_data = $client->post('adgroup-data', [
+            'id'    => $id,
+            'timezone' => Auth::user()->timezone,
             'start' => $this->dates['start']->format('Y-m-d H:i:s'),
             'end'   => $this->dates['end']->format('Y-m-d H:i:s')
         ]);
 
         $adgroup = $index_data['adgroup'];
-        $keywords = $index_data['keywords'];
-        $spend = $index_data['spend'];
-        $statistics = $index_data['statistics'];
+        $keywords = collect($index_data['keywords']);
+        $spend = collect($index_data['spend']);
+        $statistics = collect($index_data['statistics']);
 
         return view('bing::admin.adgroup')->with(compact('adgroup', 'keywords', 'spend', 'statistics'));
     }
