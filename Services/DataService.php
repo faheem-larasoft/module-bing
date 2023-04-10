bec97daa1d2a0dd26af21bb1d0bb183b7529f0b4
Faheem <faheem@larasoft.io>
2023-04-10 12:41:20 +0500

    DataService updated.

--- Services/DataService.php
+++ Services/DataService.php
@@ -4,32 +4,47 @@ namespace Modules\Bing\Services;
 
 use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class DataService
 {
     public function post($endpoint, $form = [])
     {
-        $client = new Client();
-        $response = $client->post(config('service.bing.url') . '/' . $endpoint, [
-            'form_params' => $form,
-            'headers'     => [
-                'Authorization' => 'Bearer ' . $this->jwt()
-            ]
-        ]);
+        try {
+            $client = new Client();
+            $response = $client->post(config('service.bing.url') . '/' . $endpoint, [
+                'form_params' => $form,
+                'headers'     => [
+                    'Authorization' => 'Bearer ' . $this->jwt()
+                ]
+            ]);
 
-        return $this->format($response);
+            return $this->format($response);
+
+        } catch (\Exception $e) {
+            $json_error = json_decode($e->getResponse()->getBody()->getContents());
+            $error = $json_error->message ?? 'An error occurred while fetching data from Bing service.';
+            Log::info($error);
+        }
     }
 
     public function get($endpoint, $collect = true)
     {
-        $client = new Client();
-        $response = $client->get(config('service.bing.url') . '/' . $endpoint, [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->jwt()
-            ]
-        ]);
+        try {
+            $client = new Client();
+            $response = $client->get(config('service.bing.url') . '/' . $endpoint, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->jwt()
+                ]
+            ]);
 
-        return $this->format($response, $collect);
+            return $this->format($response, $collect);
+
+        } catch (\Exception $e) {
+            $json_error = json_decode($e->getResponse()->getBody()->getContents());
+            $error = $json_error->message ?? 'An error occurred while fetching data from Bing service.';
+            Log::info($error);
+        }
     }
 
     private function format($response, $collect = true)
